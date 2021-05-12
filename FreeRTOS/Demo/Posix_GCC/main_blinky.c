d6a435ab751e7e90cbc55dd4c665bc25d3371ea2
jordan <857413240@qq.com>
2021-05-11 20:01:04 -0700

    test:when queue is full,the data waiting to send will be throw away

--- FreeRTOS/Demo/Posix_GCC/main_blinky.c
+++ FreeRTOS/Demo/Posix_GCC/main_blinky.c
@@ -12,17 +12,20 @@
 #include "console.h"
 
 
+#define	mainQUEUE_MONITOR_TASK_PRIORITY		( tskIDLE_PRIORITY + 3 )
 #define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
 #define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
 #define mainTASK_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 200UL )
+#define mainTASK_RECEIVE_FREQUENCY_MS			pdMS_TO_TICKS( 400UL )
 #define mainTIMER_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 2000UL )
-#define mainQUEUE_LENGTH					( 2 )
+#define mainQUEUE_LENGTH					( 10 )
 #define mainVALUE_SENT_FROM_TASK			( 100UL )
 #define mainVALUE_SENT_FROM_TIMER			( 200UL )
 
 static void prvQueueReceiveTask( void *pvParameters );
 static void prvQueueSendTask( void *pvParameters );
 static void prvQueueSendTimerCallback( TimerHandle_t xTimerHandle );
+static void prvQueueMonitorTask( void *pvParameters );
 
 static QueueHandle_t xQueue = NULL;
 static TimerHandle_t xTimer = NULL;
@@ -46,16 +49,18 @@ const TickType_t xTimerPeriod = mainTIMER_SEND_FREQUENCY_MS;
 
 		xTaskCreate( prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL );
 
-		xTimer = xTimerCreate( "Timer",				/* The text name assigned to the software timer - for debug only as it is not used by the kernel. */
-								xTimerPeriod,		/* The period of the software timer in ticks. */
-								pdTRUE,				/* xAutoReload is set to pdTRUE. */
-								NULL,				/* The timer's ID is not used. */
-								prvQueueSendTimerCallback );/* The function executed when the timer expires. */
+		xTaskCreate( prvQueueMonitorTask, "Monitor", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_MONITOR_TASK_PRIORITY, NULL );
 
-		if( xTimer != NULL )
-		{
-			xTimerStart( xTimer, 0 );
-		}
+		// xTimer = xTimerCreate( "Timer",				/* The text name assigned to the software timer - for debug only as it is not used by the kernel. */
+		// 						xTimerPeriod,		/* The period of the software timer in ticks. */
+		// 						pdTRUE,				/* xAutoReload is set to pdTRUE. */
+		// 						NULL,				/* The timer's ID is not used. */
+		// 						prvQueueSendTimerCallback );/* The function executed when the timer expires. */
+
+		// if( xTimer != NULL )
+		// {
+		// 	xTimerStart( xTimer, 0 );
+		// }
 
 		vTaskStartScheduler();
 	}
@@ -65,11 +70,14 @@ const TickType_t xTimerPeriod = mainTIMER_SEND_FREQUENCY_MS;
 
 
 
+#define SUCCESS 1
+#define FAIL 0
 static void prvQueueSendTask( void *pvParameters )
 {
 TickType_t xNextWakeTime;
 const TickType_t xBlockTime = mainTASK_SEND_FREQUENCY_MS;
-const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TASK;
+uint32_t ulValueToSend = 0UL;
+uint32_t ret=0;
 
 	/* Prevent the compiler warning about the unused parameter. */
 	( void ) pvParameters;
@@ -89,7 +97,15 @@ const uint32_t ulValueToSend = mainVALUE_SENT_FROM_TASK;
 		write to the console.  0 is used as the block time so the send operation
 		will not block - it shouldn't need to block as the queue should always
 		have at least one space at this point in the code. */
-		xQueueSend( xQueue, &ulValueToSend, 0U );
+
+		ulValueToSend ++;
+		ret = xQueueSend( xQueue, &ulValueToSend, 0U );
+		if(ret==SUCCESS){
+			console_print( "send success\n");
+		}else{
+			console_print( "send fail\n");
+		}
+		
 	}
 }
 
@@ -111,25 +127,53 @@ static void prvQueueSendTimerCallback( TimerHandle_t xTimerHandle )
 static void prvQueueReceiveTask( void *pvParameters )
 {
 uint32_t ulReceivedValue;
+const TickType_t xBlockTime = mainTASK_RECEIVE_FREQUENCY_MS;
+TickType_t xNextWakeTime;
 
 	/* Prevent the compiler warning about the unused parameter. */
 	( void ) pvParameters;
 
+	xNextWakeTime = xTaskGetTickCount();
+
 	for( ;; )
 	{
+
+		vTaskDelayUntil( &xNextWakeTime, xBlockTime );
 		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
 
-		if( ulReceivedValue == mainVALUE_SENT_FROM_TASK )
-		{
-			console_print( "Message received from task\n" );
-		}
-		else if( ulReceivedValue == mainVALUE_SENT_FROM_TIMER )
-		{
-			console_print( "Message received from software timer\n" );
-		}
-		else
-		{
-			console_print( "Unexpected message\n" );
-		}
+		console_print( "Message received from task:%d\n",ulReceivedValue);
+
+		// if( ulReceivedValue == mainVALUE_SENT_FROM_TASK )
+		// {
+		// 	console_print( "Message received from task\n" );
+		// }
+		// else if( ulReceivedValue == mainVALUE_SENT_FROM_TIMER )
+		// {
+		// 	console_print( "Message received from software timer\n" );
+		// }
+		// else
+		// {
+		// 	console_print( "Unexpected message\n" );
+		// }
+	}
+}
+
+
+static void prvQueueMonitorTask( void *pvParameters )
+{
+uint32_t ulReceivedValue;
+const TickType_t xBlockTime = 100UL;
+TickType_t xNextWakeTime;
+
+	/* Prevent the compiler warning about the unused parameter. */
+	( void ) pvParameters;
+
+	xNextWakeTime = xTaskGetTickCount();
+
+	for( ;; )
+	{
+
+		vTaskDelayUntil( &xNextWakeTime, xBlockTime );
+		console_print( "Queue Messages Waiting:%ld\n",uxQueueMessagesWaiting(xQueue));
 	}
 }
